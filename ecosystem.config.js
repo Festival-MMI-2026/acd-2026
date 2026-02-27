cb6e050322536ad2c21676836ce29a9fbb23ac15
audricfullhardt <fullhardtaudric@gmail.com>
2026-02-27 17:04:32 +0100

    chore: add pm2 for node server

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,15 +1,15 @@
 module.exports = {
-    apps: [
-      {
-        name: "acd-preprod",
-        script: ".output/server/index.mjs",
-        exec_mode: "fork",
-        instances: 1,
-        env: {
-          NODE_ENV: "production",
-          PORT: 3000
-        }
+  apps: [
+    {
+      name: "acd-preprod",
+      script: ".output/server/index.mjs",
+      cwd: "/var/www/acd-preprod",
+      exec_mode: "fork",
+      instances: 1,
+      env: {
+        NODE_ENV: "production",
+        PORT: 3000
       }
-    ]
-  }
-  
+    }
+  ]
+}
